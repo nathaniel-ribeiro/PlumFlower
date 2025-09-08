578f9aefcc5243b224c6ec93dfa56bb7e011f240
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-09-07 22:28:11 -0400

    batch processing games

--- data_processing.py
+++ data_processing.py
@@ -7,6 +7,7 @@ import config
 from tqdm import tqdm
 import multiprocessing as mp
 from multiprocessing import Pool, cpu_count
+import time
 
 # extract all PGN game files
 result = subprocess.run(f"find {config.DATA_DIR} -type f -name '*.pgns'", shell=True, check=True, capture_output=True)
@@ -38,31 +39,37 @@ for filename in filenames:
         game = Game(moves, *metadata.values())
         games.append(game)
 
-#TODO: remove this! Just for testing!
+#TODO: remove this, just for testing
 games = games[:100]
-
 print("Starting annotations...")
 
 num_workers = cpu_count()
 
-def worker(me):
+def worker(games_batch):
     engine = PikafishEngine()
     boards, evaluations = list(), list()
-    for i, game in enumerate(games):
-        if i % num_workers != me:
-            continue
-        board, evaluation = annotate(game, engine=engine, think_time=100)
+    for game in games_batch:
+        board, evaluation = annotate(game, engine=engine, think_time=500)
         boards.extend(board)
         evaluations.extend(evaluation)
-        print("Worker {me} finished annotating game #{i}")
     engine.quit()
     return boards, evaluations
 
-all_boards, all_evaluations = list(), list()
-with Pool(num_workers) as pool:
-    for boards, evaluations in tqdm(pool.map(worker, list(range(num_workers))), total = len(games)):
-        all_boards.extend(boards)
-        all_evaluations.extend(evaluations)
+batch_size = 10
+batches = [games[i:i+batch_size] for i in range(0, len(games), batch_size)]
 
-df = pd.DataFrame({'FEN': all_boards, 'Evaluation': evaluations})
-df.to_csv('annotated_games.csv', index=False)
+tick = time.time()
+with Pool(num_workers) as pool:
+    for boards, evaluations in pool.imap_unordered(worker, batches):
+        df = pd.DataFrame({'FEN': boards, 'Evaluation': evaluations})
+        df.to_csv(
+            'annotated_games.csv',
+            mode='a',
+            header=not pd.io.common.file_exists('annotated_games.csv'),
+            index=False
+        )
+
+tock = time.time()
+total_time = tock - tick
+average_time = total_time / len(games)
+print(f"Projected total time: {average_time * 150000} s OR {average_time * 2500} mins OR {average_time * 41.67} hours")
