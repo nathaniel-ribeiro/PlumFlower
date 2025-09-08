3236627adc536fc697cdecfa5f10fac36809af1a
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-09-07 21:05:14 -0400

    beginning game annotations (debugged issues)

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,12 @@
 import subprocess
 from game import Game
 import re
-from annotate import annotate
+from annotate import annotate, PikafishEngine
 import pandas as pd
 import config
+from tqdm import tqdm
+import multiprocessing as mp
+from multiprocessing import Pool, cpu_count
 
 # extract all PGN game files
 result = subprocess.run(f"find {config.DATA_DIR} -type f -name '*.pgns'", shell=True, check=True, capture_output=True)
@@ -16,7 +19,8 @@ for filename in filenames:
     
     games_info = [game_info.strip() for game_info in contents.split('\n\n') if game_info.strip()]
 
-    for game_info in games_info:
+    print("Importing PGN files...")
+    for game_info in tqdm(games_info):
         # extract metadata such as player names and event date
         metadata_matches = re.findall(r'^\[(\w+)\s+"(.*?)"\]$', game_info, flags=re.MULTILINE)
         metadata = {key: value for key, value in metadata_matches}
@@ -34,13 +38,31 @@ for filename in filenames:
         game = Game(moves, *metadata.values())
         games.append(game)
 
-# convert games to board states with annotations
-all_board_states, all_evaluations = list(), list()
-for game in games:
-    board_states, evaluations = annotate(game)
-    all_board_states.extend(board_states)
-    all_evaluations.extend(evaluations)
-    break
+#TODO: remove this! Just for testing!
+games = games[:100]
 
-df = pd.DataFrame({'FEN Board': board_states, 'Evaluation': evaluations})
-df.to_csv('annotated_games.csv')
+print("Starting annotations...")
+
+num_workers = cpu_count()
+
+def worker(me):
+    engine = PikafishEngine()
+    boards, evaluations = list(), list()
+    for i, game in enumerate(games):
+        if i % num_workers != me:
+            continue
+        board, evaluation = annotate(game, engine=engine, think_time=100)
+        boards.extend(board)
+        evaluations.extend(evaluation)
+        print("Worker {me} finished annotating game #{i}")
+    engine.quit()
+    return boards, evaluations
+
+all_boards, all_evaluations = list(), list()
+with Pool(num_workers) as pool:
+    for boards, evaluations in tqdm(pool.map(worker, list(range(num_workers))), total = len(games)):
+        all_boards.extend(boards)
+        all_evaluations.extend(evaluations)
+
+df = pd.DataFrame({'FEN': all_boards, 'Evaluation': evaluations})
+df.to_csv('annotated_games.csv', index=False)
