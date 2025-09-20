ee0dd62000d072f976bdc8c3dfca7050ea7a18a2
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-09-20 15:23:31 -0400

    added troubleshooting instructions

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 from multiprocessing import cpu_count
 import math
 
-# too many engines will cause OOM errors due to overhead of hash tables + NNUE
-NUM_WORKERS = 16
+# too many engines will cause OOM errors in SLURM
+NUM_WORKERS = 8
 # path to executable to run pikafish engine. File needs execution permissions and compiler used to build must be available!
 PATH_TO_PIKAFISH_BINARY = os.path.expanduser("~/Pikafish/src/pikafish")
 # directory where PGN files for training and CSV of board states with annotations should be saved
@@ -11,4 +11,4 @@ DATA_DIR = "./data"
 # time that engine will think before producing the best move. Deepmind used 50 ms
 PIKAFISH_MOVETIME_MS = 50
 # Stockfish/Pikafish recommends num_cores * 2 - 1
-PIKAFISH_THREADS = (cpu_count() * 2) // NUM_WORKERS
+PIKAFISH_THREADS = 4
