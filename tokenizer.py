3d108b1a8e5fe32b99522ea676326c32e1cb12fa
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-10-01 21:47:19 -0400

    major bugfixes to tokenization (simpler handling of rows, only use regex for metadata)

--- tokenizer.py
+++ tokenizer.py
@@ -1,25 +1,31 @@
 import re
 
+FEN_METADATA_REGEX = re.compile(
+    r"^([wb]) - - (\d+) (\d+)$"
+)
+
 class BoardTokenizer:
     def __init__(self):
         pass
 
-    def encode(fen_batch):
+    def encode(self, fen_batch):
         tokenized_batch = [None] * len(fen_batch)
         for i, fen in enumerate(fen_batch):
-            match = re.search(r'([rnbakcRNBAKC0-9]/){9}([rnbakcRNBAKC0-9]) ([wb]) - - (\d+) (\d+)', fen)
-            tokenized = None
-            if match:
-                rows = "".join([re.sub(r'\d', lambda match: "." * int(match), match.group(i)) for i in range(10)])
-                # uses e and E to denote black and red elephants rather than b (already used in FEN to show black to move)
-                rows = rows.replace("b", "e")
-                rows = rows.replace("B", "E")
-                
-                whose_turn = match.group(10)
-                # left pad with zeros to ensure a fixed length
-                capture_clock = match.group(11).zfill(2)
-                halfmove_clock = match.group(12).zfill(3)
-                tokenized =  list(rows) + list(whose_turn) + list(capture_clock) + list(halfmove_clock)
+            board, metadata = fen.split(" ", 1)
+            rows = board.split("/")
+            # replace digit n with n empty square tokens
+            rows = "".join([re.sub(r'\d', lambda m: "." * int(m.group(0)), row) for row in rows])
+            # use e and E for black and red elephants to disambiguate from whose turn to move
+            rows = rows.replace("b", "e").replace("B", "E")
             
+            m = FEN_METADATA_REGEX.match(metadata)
+            whose_move = m.group(1)
+            # left pad with zeros to ensure fixed length
+            capture_clock = m.group(2).zfill(2)
+            halfmove_clock = m.group(3).zfill(3)
+            
+            tokenized = list(rows) + list(whose_move) + list(capture_clock) + list(halfmove_clock)
+            # 90 squares + 1 token whose move + 2 digits for capture clock + 3 digits for halfmove clock = 95 tokens
+            assert len(tokenized) == 96
             tokenized_batch[i] = tokenized
         return tokenized_batch
