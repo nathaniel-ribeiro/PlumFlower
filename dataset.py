18d1ce36d3682546ad3de7365a1563ad82b7fdba
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-10-03 20:27:49 -0400

    minor typo fix, switched to returning torch tensor for evaluations

--- dataset.py
+++ dataset.py
@@ -1,22 +1,19 @@
-import torch.nn as nn
-import torch.utils.data.Dataset as Dataset
+import torch
 import pandas as pd
 import config
 from tokenizer import BoardTokenizer
+import numpy as np
 
-class XQAnnotatedDataset(Dataset):
-    def __init__(self, path_to_csv, tokenizer):
+class AnnotatedBoardsDataset(torch.utils.data.Dataset):
+    def __init__(self, path_to_csv):
         self.df = pd.read_csv(path_to_csv)
-        self.tokenizer = tokenizer
 
     def __len__(self):
         return len(self.df)
     
-    def __get_item__(self, idx):
+    def __getitem__(self, idx):
         row = self.df.iloc[idx]
         _, fen, _, win_prob, draw_prob, lose_prob = tuple(row)
-        fen_tokenized = self.tokenizer.encode(fen)
-        return fen_tokenized, (win_prob, draw_prob, lose_prob)
-
-train_ds = XQAnnotatedDataset(f'{config.DATA_DIR}/train.csv', BoardTokenizer())
-print(train_ds[0])
+        evaluation = np.array([win_prob, draw_prob, lose_prob], dtype=np.float32)
+        evaluation = torch.from_numpy(evaluation)
+        return fen, evaluation
