55aa5a91ddb11b298d5e8b8ecf0bb5dc26167650
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-10-03 20:04:54 -0400

    splitting dataset into folds and deduplicating within folds

--- train_test_split.py
+++ train_test_split.py
@@ -1,7 +1,26 @@
 import math
 import polars as pl
-from sklearn.model_selection import GroupShuffleSplit
+from sklearn.model_selection import train_test_split
 import pandas as pd
+import config
 
-df = pd.read_csv("data/annotated_games.csv")
-print(df.info(memory_usage='deep'))
+if __name__ == "__main__":
+    df = pd.read_csv(f"{config.DATA_DIR}/annotated_games.csv")
+
+    unique_game_ids = df['Game ID'].unique()
+    # split game ids into 80% train, 10% val, 10% test
+    train_ids, val_test_ids = train_test_split(unique_game_ids, test_size=0.2, random_state=42)
+    val_ids, test_ids = train_test_split(val_test_ids, test_size=0.5, random_state=42)
+
+    train_df = df[df['Game ID'].isin(train_ids)]
+    val_df = df[df['Game ID'].isin(val_ids)]
+    test_df = df[df['Game ID'].isin(test_ids)]
+
+    # remove duplicate boards within folds but NOT across folds
+    train_df.drop_duplicates(subset=['FEN'], inplace=True)
+    val_df.drop_duplicates(subset=['FEN'], inplace=True)
+    test_df.drop_duplicates(subset=['FEN'], inplace=True)
+
+    train_df.to_csv(f'{config.DATA_DIR}/train.csv')
+    val_df.to_csv(f'{config.DATA_DIR}/val.csv')
+    test_df.to_csv(f'{config.DATA_DIR}/test.csv')
