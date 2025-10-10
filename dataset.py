75917782796f3b2bba311b4bc852580c656e4597
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-10-09 20:05:04 -0400

    no longer tokenizing in batches, converting tokens to token indices for embedding lookup later

--- dataset.py
+++ dataset.py
@@ -4,10 +4,27 @@ import numpy as np
 from tokenizer import BoardTokenizer
 
 class AnnotatedBoardsDataset(torch.utils.data.Dataset):
-    def __init__(self, path_to_csv, board_flip_probability=0.0):
+    def __init__(self, path_to_csv, board_flip_probability=0.0, mask_token_probability=0.0):
         self.df = pd.read_csv(path_to_csv)
         self.tokenizer = BoardTokenizer()
         self.board_flip_probability = board_flip_probability
+        self.mask_token_probability = mask_token_probability
+        self.vocab = ['K', 'A', 'E', 'R', 'C', 'N', 'P',
+                      'k', 'a', 'e', 'r', 'c', 'n', 'p',
+                      '0', '1', '2', '3', '4', '5', '6',
+                      '7', '8', '9', '0', 'w', 'b', '.',
+                      '[MASK]']
+        
+        self.vocab_to_idx = dict(zip(self.vocab, range(len(self.vocab))))
+    
+    def horizontal_flip(self, fen):
+        print(fen)
+        board, metadata = fen.split(" ", 2)
+        rows = board.split("/")
+        rows_flipped = [row.reverse() for row in rows]
+        board_flipped = "/".join(rows_flipped)
+        fen_flipped = board_flipped + " " + metadata
+        return fen_flipped
 
     def __len__(self):
         return len(self.df)
@@ -19,16 +36,23 @@ class AnnotatedBoardsDataset(torch.utils.data.Dataset):
         evaluation = np.array([win_prob, draw_prob, lose_prob], dtype=np.half)
         evaluation = torch.from_numpy(evaluation)
 
-        if np.random.rand() <= self.board_flip_probability:
-            fen = self.horizontal_flip(fen)
+        # if np.random.rand() <= self.board_flip_probability:
+        #     fen = self.horizontal_flip(fen)
 
         fen_tokenized = self.tokenizer.encode(fen)
-        return fen_tokenized, evaluation
-    
-    def horizontal_flip(fen):
-        board, metadata = fen.split(" ", 1)
-        rows = board.split("/")
-        rows_flipped = [row.reverse() for row in rows]
-        board_flipped = "/".join(rows_flipped)
-        fen_flipped = board_flipped + " " + metadata
-        return fen_flipped
+        
+        if self.mask_token_probability > 0.0:
+            for i, token in enumerate(fen_tokenized):
+                # don't mask these since the WDL probs are from current player perspective
+                if token == "w" or token == "b":
+                    continue
+                fen_tokenized[i] = "[MASK]" if np.random.rand() <= self.mask_token_probability else fen_tokenized[i]
+        
+        # convert tokens to their corresponding indices in the vocab, uint8 ok here since |V| \leq 256
+        fen_tokenized_indices = np.array([self.vocab_to_idx[token] for token in fen_tokenized], dtype=np.uint8)
+        fen_tokenized_indices = torch.from_numpy(fen_tokenized_indices)
+        return fen_tokenized_indices, evaluation
+
+if __name__ == "__main__":
+    ds = AnnotatedBoardsDataset("./data/train.csv", 0.5, 0.0)
+    print(ds[0])
--- tokenizer.py
+++ tokenizer.py
@@ -4,24 +4,21 @@ class BoardTokenizer:
     def __init__(self):
         self.metadata_regex = re.compile(r"^([wb]) - - (\d+) (\d+)$")
 
-    def encode(self, fen_batch):
-        tokenized_batch = [None] * len(fen_batch)
-        for i, fen in enumerate(fen_batch):
-            board, metadata = fen.split(" ", 1)
-            rows = board.split("/")
-            # replace digit n with n empty square tokens
-            rows = "".join([re.sub(r'\d', lambda m: "." * int(m.group(0)), row) for row in rows])
-            # use e and E for black and red elephants to disambiguate from whose turn to move
-            rows = rows.replace("b", "e").replace("B", "E")
+    def encode(self, fen):
+        board, metadata = fen.split(" ", 1)
+        rows = board.split("/")
+        # replace digit n with n empty square tokens
+        rows = "".join([re.sub(r'\d', lambda m: "." * int(m.group(0)), row) for row in rows])
+        # use e and E for black and red elephants to disambiguate from whose turn to move
+        rows = rows.replace("b", "e").replace("B", "E")
             
-            m = self.metadata_regex.match(metadata)
-            whose_move = m.group(1)
-            # left pad with zeros to ensure fixed length
-            capture_clock = m.group(2).zfill(2)
-            halfmove_clock = m.group(3).zfill(3)
+        m = self.metadata_regex.match(metadata)
+        whose_move = m.group(1)
+        # left pad with zeros to ensure fixed length
+        capture_clock = m.group(2).zfill(2)
+        halfmove_clock = m.group(3).zfill(3)
             
-            tokenized = list(rows) + list(whose_move) + list(capture_clock) + list(halfmove_clock)
-            # 90 squares + 1 token whose move + 2 digits for capture clock + 3 digits for halfmove clock = 96 tokens
-            assert len(tokenized) == 96
-            tokenized_batch[i] = tokenized
-        return tokenized_batch
+        tokenized = list(rows) + list(whose_move) + list(capture_clock) + list(halfmove_clock)
+        # 90 squares + 1 token whose move + 2 digits for capture clock + 3 digits for halfmove clock = 96 tokens
+        assert len(tokenized) == 96
+        return tokenized
